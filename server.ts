fd8b381fc97310c0e6c9911eabb1a33f82b2721a
Andrew Shek <andrew@tecky.io>
2023-05-30 12:07:45 +0800

    Home Page

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 dotenv.config();
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname,"public")));
 app.listen(process.env.PORT,()=>{
     console.log("Online Book Store Server is started");
 });
