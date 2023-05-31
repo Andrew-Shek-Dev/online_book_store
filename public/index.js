c54765440cdf662a8ec506636c2627dea068b636
Andrew Shek <andrew@tecky.io>
2023-05-31 11:50:54 +0800

    Login Ready

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
+const BACKEND_SERVER_URL = "https://dummyjson.com/";
 async function loadBooks(){
     //Ask for server - fetch
-    const res = await fetch("https://dummyjson.com/products");
+    const res = await fetch(BACKEND_SERVER_URL+"products");
     //Extract JSON
     const books = await res.json();
     //Update DOM
@@ -16,7 +17,45 @@ async function loadBooks(){
     }
 }
 
+async function loginFormSetup(){
+    document.querySelector("form#loginPanel").addEventListener("submit",async (event)=>{
+        event.preventDefault();
+        const form = event.target;
+        const res = await fetch(BACKEND_SERVER_URL+"auth/login",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                username: form.username.value /*'kminchelle'*/,
+                password: form.password.value/*'0lelplR'*/,
+            })
+        });
+        const user = await res.json();
+        if (user){
+            const loginButton = document.querySelector("button#loginButton");
+            const userInfo = document.querySelector("div#logginPanel");
+            loginButton.setAttribute("hidden",undefined);
+            userInfo.removeAttribute("hidden");
+            const span = userInfo.querySelector("span");
+            span.innerText = user.username;
+        }
+    })
+}
+
+async function logoutFormSetup(){
+    document.querySelector("button#logout").addEventListener("click",async(event)=>{
+        //TODO: fetch POST /logout
+        const loginButton = document.querySelector("button#loginButton");
+        const userInfo = document.querySelector("div#logginPanel");
+        userInfo.setAttribute("hidden",undefined);
+        loginButton.removeAttribute("hidden");
+    });
+}
+
 const main = async ()=>{
     await loadBooks();
+    await loginFormSetup();
+    await logoutFormSetup();
 }
 main();
